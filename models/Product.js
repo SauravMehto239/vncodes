dd9efc044208670a820cc62bc31949b9dceeefb0
SauravMehto239 <webdesign1@paisalo.in>
2025-08-07 12:10:12 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true }, // stores path or URL
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', productSchema); 
