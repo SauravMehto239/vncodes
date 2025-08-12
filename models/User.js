26bff526ffdb56e3ae0befe76391ddee9f3f6811
SauravMehto239 <webdesign1@paisalo.in>
2025-08-13 03:16:09 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  mobileNumber: { type: String, required: true, trim: true },
+  country: { type: String, required: true, trim: true },
+  state: { type: String, required: true, trim: true },
+  city: { type: String, required: true, trim: true },
+  zipCode: { type: String, required: true, trim: true },
   createdAt: { type: Date, default: Date.now },
   lastLogin: { type: Date }
 });
